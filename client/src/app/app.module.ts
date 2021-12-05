1b4e595e9ce60528d3f0eb8ed3318669251195b8
johnram528 <johnram528@gmail.com>
2021-12-05 15:41:16 -0600

    add chat screen component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChatComponent } from './views/chat/chat.component';
 import { ChatInputComponent } from './components/chat-input/chat-input.component';
+import { ChatScreenComponent } from './components/chat-screen/chat-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
-    ChatInputComponent
+    ChatInputComponent,
+    ChatScreenComponent
   ],
   imports: [
     BrowserModule,
