708987e0157c8545aa94f5451769f6028480c12c
johnram528 <johnram528@gmail.com>
2021-12-05 16:19:10 -0600

    initial chat input styling

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { ChatComponent } from './views/chat/chat.component';
 import { ChatInputComponent } from './components/chat-input/chat-input.component';
 import { ChatScreenComponent } from './components/chat-screen/chat-screen.component';
@@ -16,7 +19,10 @@ import { ChatScreenComponent } from './components/chat-screen/chat-screen.compon
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
