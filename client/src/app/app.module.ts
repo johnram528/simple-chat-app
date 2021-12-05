d5815a2238b7d6f62e82a36b1d7a80265abc1bd7
johnram528 <johnram528@gmail.com>
2021-12-05 15:38:07 -0600

    add chat input component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChatComponent } from './views/chat/chat.component';
+import { ChatInputComponent } from './components/chat-input/chat-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    ChatInputComponent
   ],
   imports: [
     BrowserModule,
