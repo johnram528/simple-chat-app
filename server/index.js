b3211cfe6129800927a8fde47f6a68c2ab2717d4
johnram528 <johnram528@gmail.com>
2021-12-06 21:16:04 -0600

    add uuid and datetime to message obj

--- server/index.js
+++ server/index.js
@@ -3,12 +3,18 @@ const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer, {
   cors: {origin : '*'}
 });
+const { v4: uuidv4 } = require('uuid');
 
 io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('message', (message) => {
-    console.log(message);
-    io.emit('message', message);
+    const updateMsg = {
+			...message,
+			id: uuidv4(),
+			datetime: new Date()
+		}
+    console.log(updateMsg);
+    io.emit('message', updateMsg);
   });
 
   socket.on('disconnect', () => {
